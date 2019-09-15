26e2df6fce205ba2a78f71291d3346283decfc6d
alieti <8minan@gmail.com>
2019-09-15 23:12:07 +0200

    ninth commit

--- testlab/R/euclidean.R
+++ testlab/R/euclidean.R
@@ -1,10 +1,12 @@
 #' This is the Euclidean function
-#' @param A
-#' @param B
+#' @param a a number
+#' @param b a number
 #' 
-#' @return the GCD of two given numbers
-#' @example Euclidean(100,10)
-#' @description This is a great function rarely seen in nature. See here: https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=12&cad=rja&uact=8&ved=2ahUKEwj6nP7jsdHkAhVBtIsKHUfVCM8QFjALegQIABAB&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEuclidean_algorithm&usg=AOvVaw2vg5R_wmq5L2pEWXWyvzB1
+#' @return the GCD of a and b.
+#' @examples  
+#' \code{euclidean(a, b)}
+#' @description This is a great function rarely seen in nature. See here: 
+#' \url {https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=12&cad=rja&uact=8&ved=2ahUKEwj6nP7jsdHkAhVBtIsKHUfVCM8QFjALegQIABAB&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEuclidean_algorithm&usg=AOvVaw2vg5R_wmq5L2pEWXWyvzB1}
 
 euclidean <- function(a,b){
   #' check input type
